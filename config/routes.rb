12ca1abd365b96e2b69ba7a2367717c0c22febea
DuyTruyen1 <truyenmap420@gmail.com>
2025-06-13 01:00:29 +0700

    Section 8.1.1 - Generate Sessions controller with login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "sessions/new"
   root "static_pages#home"
   get "/home",    to: "static_pages#home"
   get "/help",    to: "static_pages#help"
@@ -6,4 +7,7 @@ Rails.application.routes.draw do
   get "/contact", to: "static_pages#contact"
   get "/signup",  to: "users#new"
   resources :users
+  get    "/login",   to: "sessions#new"
+  post   "/login",   to: "sessions#create"
+  delete "/logout",  to: "sessions#destroy"
 end
