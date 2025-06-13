49b856332577a0afd8fd6a4b6f2898e26047a2ce
DuyTruyen1 <truyenmap420@gmail.com>
2025-06-13 11:05:34 +0700

    Add User.digest method for test fixtures

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,11 @@ class User < ApplicationRecord
 
   has_secure_password
 
+   def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
   private
 
   def email_must_not_have_consecutive_dots
