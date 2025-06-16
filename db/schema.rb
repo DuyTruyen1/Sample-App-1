739dc4623dec29b286cbcc5f3480e046921f88dd
DuyTruyen1 <truyenmap420@gmail.com>
2025-06-16 13:31:06 +0700

    Implement basic remember me functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_12_195856) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_16_062647) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "remember_digest"
   end
 end
