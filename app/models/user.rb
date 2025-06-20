a25fd26abac74fc0c476e8d1da841122b0a9dee7
DuyTruyen1 <truyenmap420@gmail.com>
2025-06-20 10:59:26 +0700

    fix:line break user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,9 @@ class User < ApplicationRecord
 
   before_save { self.email = email.downcase }
 
-  validates :name, presence: true, length: { maximum: 50 },
-                   format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters and spaces" }
+  validates :name, presence: true, length: { maximum: 50 }, format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters and spaces" }
 
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
   validates :password, presence: true, length: { minimum: 6, maximum: 72 }
 
