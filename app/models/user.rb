cd3df0e6388228eedcff3dd71e7c9c64a5f9ede3
DuyTruyen1 <truyenmap420@gmail.com>
2025-06-22 14:18:31 +0700

    fix user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,16 @@
 class User < ApplicationRecord
-  before_save :downcase_email
-
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
+  before_save { self.email = email.downcase }
+
+  validates :name, presence: true, length: { maximum: 50 }, format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters and spaces" }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6, maximum: 72 }
   validate :email_must_not_have_consecutive_dots
 
   has_secure_password
 
   private
-
-  def downcase_email
-    self.email = email.downcase
-  end
 
   def email_must_not_have_consecutive_dots
     if email.present? && email.match?(/\.\./)
