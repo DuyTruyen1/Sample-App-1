1cb28c51fbf1c988b6e18996aaf5ef786a0f837f
DuyTruyen1 <truyenmap420@gmail.com>
2025-06-22 14:01:04 +0700

    fix: after review

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,9 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-  validates :name,
-            presence: true,
-            length: { maximum: 50 },
-            format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters and spaces" }
-
-  validates :email,
-            presence: true,
-            length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false }
-
-  validates :password,
-            presence: true,
-            length: { minimum: 6, maximum: 72 }
-
-  validate :email_must_not_have_consecutive_dots
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 
   has_secure_password
 
